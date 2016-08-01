3b45c12a3edec8839dba49230635b0eb8fa07cfe
yash <yashgulati.g1@gmail.com>
2016-08-01 12:18:51 +0530

    added frameworks and header, footer

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>AtomCMS 2.0</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css\bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css\bootstrap-theme-min.css" media="screen" title="no title" charset="utf-8">
+    <link rel="stylesheet" href="css\jquery-ui.min.css" media="screen" title="no title" charset="utf-8">
+    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.6.3/css/font-awesome.css" media="screen" title="no title" charset="utf-8"> -->
+
+    <script type="text/javascript" src="js\jquery-1.8.0.min.js"></script>
+    <script type="text/javascript" src="js\jquery-ui.min.js"></script>
+    <script type="text/javascript" src="js\bootstrap.min.js"></script>
+
+    <style media="screen">
+    html {
+position: relative;
+min-height: 100%;
+}
+body {
+/* Margin bottom by footer height */
+margin-bottom: 60px;
+}
+.footer {
+position: absolute;
+bottom: 0;
+width: 100%;
+/* Set the fixed height of the footer here */
+height: 60px;
+background-color: #f5f5f5;
+}
+    </style>
+  </head>
+  <body>
+  <nav class="navbar navbar-default" role="navigation">
+    <div class="container">
+      <ul class="nav navbar-nav">
+        <li class="active"><a href="#">Home</a></li>
+        <li><a href="#">About Us</a></li>
+        <li><a href="#">FAQ</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </div>
+    </nav>
+
+  <div class="container">
+    <h1>Content</h1>
+  </div>
+
+  <div class="footer">
+    <div class="container">
+      <p>
+        This is the footer.
+      </p>
+    </div>
+  </div>
+  </body>
+</html>
