e33d20d6e83520075ed440619379b8baae617ade
yash <yashgulati.g1@gmail.com>
2016-08-01 12:31:17 +0530

    done with css.php but not js.php in config

---
+++ config/css.php
@@ -0,0 +1,24 @@
+
+<link rel="stylesheet" href="css\bootstrap.min.css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="css\bootstrap-theme-min.css" media="screen" title="no title" charset="utf-8">
+<link rel="stylesheet" href="css\jquery-ui.min.css" media="screen" title="no title" charset="utf-8">
+<!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.6.3/css/font-awesome.css" media="screen" title="no title" charset="utf-8"> -->
+
+<style media="screen">
+html {
+position: relative;
+min-height: 100%;
+}
+body {
+/* Margin bottom by footer height */
+margin-bottom: 60px;
+}
+.footer {
+position: absolute;
+bottom: 0;
+width: 100%;
+/* Set the fixed height of the footer here */
+height: 60px;
+background-color: #f5f5f5;
+}
+</style>
--- index.php
+++ index.php
@@ -4,33 +4,12 @@
     <meta charset="utf-8">
     <title>AtomCMS 2.0</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="css\bootstrap.min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="css\bootstrap-theme-min.css" media="screen" title="no title" charset="utf-8">
-    <link rel="stylesheet" href="css\jquery-ui.min.css" media="screen" title="no title" charset="utf-8">
-    <!-- <link rel="stylesheet" href="https://cdn.jsdelivr.net/fontawesome/4.6.3/css/font-awesome.css" media="screen" title="no title" charset="utf-8"> -->
-
+    <?php include('config/css.php'); ?>
     <script type="text/javascript" src="js\jquery-1.8.0.min.js"></script>
     <script type="text/javascript" src="js\jquery-ui.min.js"></script>
     <script type="text/javascript" src="js\bootstrap.min.js"></script>
 
-    <style media="screen">
-    html {
-position: relative;
-min-height: 100%;
-}
-body {
-/* Margin bottom by footer height */
-margin-bottom: 60px;
-}
-.footer {
-position: absolute;
-bottom: 0;
-width: 100%;
-/* Set the fixed height of the footer here */
-height: 60px;
-background-color: #f5f5f5;
-}
-    </style>
+
   </head>
   <body>
   <nav class="navbar navbar-default" role="navigation">
